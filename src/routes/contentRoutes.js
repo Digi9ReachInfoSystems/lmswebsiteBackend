151fb023f14a022f1a8095c615d5ee0ea10dda01
Flutterneu <rsmx141@gmail.com>
2024-11-05 18:58:06 +0530

    content Controller Updated

--- src/routes/contentRoutes.js
+++ src/routes/contentRoutes.js
@@ -1,29 +1,36 @@
-// src/routes/contentRoutes.js
-const express = require('express');
-const multer = require('multer'); // Middleware for handling form-data
+const express = require("express");
+const multer = require("multer");
 const {
   uploadContent,
   getAllContent,
   getContentByTeacherId,
-  getContentByBatchId
-} = require('../controllers/contentController');
-const authMiddleware = require('../middlewares/authMiddleware');
+  getContentByBatchId,
+} = require("../controllers/contentController");
+const authMiddleware = require("../middlewares/authMiddleware");
 const authorizeRole = require("../middlewares/authorizeRole");
 
 const router = express.Router();
-const upload = multer({ dest: 'uploads/' }); // Temporary file storage
+
+// Use memory storage to avoid writing to the local file system
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
 
 // Route to upload content
-router.post('/upload',authMiddleware,authorizeRole("teacher"), upload.single('file'), uploadContent);
+router.post(
+  "/upload",
+  authMiddleware,
+  authorizeRole("teacher"),
+  upload.single("file"),
+  uploadContent
+);
 
 // Route to get all content
-router.get('/', getAllContent);
-
+router.get("/", getAllContent);
 
 // Route to get content by teacherId
-router.get('/teacher/:teacherId', getContentByTeacherId);
+router.get("/teacher/:teacherId", getContentByTeacherId);
 
 // Route to get content by batchId
-router.get('/batch/:batchId', getContentByBatchId);
+router.get("/batch/:batchId", getContentByBatchId);
 
 module.exports = router;
