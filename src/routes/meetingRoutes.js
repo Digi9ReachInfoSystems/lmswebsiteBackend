66415d72c127f5c0f7cb1abf412b81ff05a46f09
jayanthbr29 <jayanthbr@digi9.co.in>
2024-11-22 16:37:14 +0530

    meeting api ,routes

--- src/routes/meetingRoutes.js
+++ src/routes/meetingRoutes.js
@@ -1,28 +1,66 @@
+// const express = require("express");
+// const router = express.Router();
+// const meetingController = require("../controllers/meetingController");
+// const authMiddleware = require("../middlewares/authMiddleware");
+// const authorizeRole = require("../middlewares/authorizeRole");
+
+// router.get(
+//   "/getMeetings",
+//   authMiddleware,
+//   authorizeRole("admin"),
+//   meetingController.getMeetings
+// );
+
+// router.get(
+//   "/getMeetingById/:meetingId",
+//   authMiddleware,
+//   authorizeRole("teacher"),
+//   meetingController.getMeetingsForTeacher
+// );
+
+// router.get(
+//   "/getMeetingByIdForStudent/:meetingId",
+//   authMiddleware,
+//   authorizeRole("student"),
+//   meetingController.getMeetingForStudents
+// );
+
+// module.exports = router;
+
 const express = require("express");
 const router = express.Router();
 const meetingController = require("../controllers/meetingController");
 const authMiddleware = require("../middlewares/authMiddleware");
 const authorizeRole = require("../middlewares/authorizeRole");
-
+ 
 router.get(
   "/getMeetings",
   authMiddleware,
   authorizeRole("admin"),
   meetingController.getMeetings
 );
-
+ 
 router.get(
   "/getMeetingById/:meetingId",
   authMiddleware,
   authorizeRole("teacher"),
   meetingController.getMeetingsForTeacher
 );
-
+ 
+router.post(
+  "/createMeeting",
+ 
+  meetingController.createMeetingTeams
+);
+ 
 router.get(
   "/getMeetingByIdForStudent/:meetingId",
   authMiddleware,
   authorizeRole("student"),
   meetingController.getMeetingForStudents
 );
-
+ 
+router.post("/joinmeeting", meetingController.getJoinUrl);
+ 
 module.exports = router;
+ 
