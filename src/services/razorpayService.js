8499ed7b9a86c100f635832dcdd47259f46d22b3
jayanthbr29 <jayanthbr@digi9.co.in>
2024-11-26 23:17:51 +0530

    payment api testing -v1

--- src/services/razorpayService.js
+++ src/services/razorpayService.js
@@ -1,7 +1,8 @@
 const Razorpay = require('razorpay')
 const razorpayInstance = new Razorpay({
     key_id: 'rzp_test_YW49ucyVtjePLT',
-    key_secret: 'FFn6tCkoKKQxfdvPCFuiijRE'
+    key_secret: 'FFn6tCkoKKQxfdvPCFuiijRE',
+    course_payment:'FPs-kRnkuFXq8tG-course-Payment'
 })
 
 module.exports = razorpayInstance;
