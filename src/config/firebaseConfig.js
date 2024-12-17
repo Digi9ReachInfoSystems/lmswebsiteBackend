cc2914799fdf2930f23c977bcd81ab9afe333fba
jayanthbr29 <jayanthbr@digi9.co.in>
2024-12-17 12:20:27 +0530

    teacher flow

--- src/config/firebaseConfig.js
+++ src/config/firebaseConfig.js
@@ -1,11 +1,9 @@
- const firebaseConfig = {
-    apiKey: "AIzaSyDZF4e9nAL17nGTaTT9Tehmht9M73HsaR4",
-    authDomain: "lmseducationplaform.firebaseapp.com",
-    projectId: "lmseducationplaform",
-    storageBucket: "lmseducationplaform.appspot.com",
-    messagingSenderId: "791494608780",
-    appId: "1:791494608780:web:19537fa099d9bf6f8238a9",
-  };
+// var admin = require("firebase-admin");
 
+// var serviceAccount = require("../utils/lmseducationplaform-firebase-adminsdk-yxflg-1ded6b0f95.json");
 
-  module.exports = firebaseConfig;
+// admin.initializeApp({
+//   credential: admin.credential.cert(serviceAccount)
+// });
+
+// module.exports = admin;
