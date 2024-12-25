a6be42ce38b2d098470abf49bd08b080f01f59c2
jayanthbr29 <jayanthbr@digi9.co.in>
2024-12-25 18:12:13 +0530

    vercel cron job configuration

--- src/controllers/cronController.js
+++ src/controllers/cronController.js
@@ -1,5 +1,6 @@
 const connectDB = require("../config/database");
 const manageExpiredBatches = require("../Jobs/completeBatchExpiryJob.js");
+const scheduleBatchExpiryJob = require("../Jobs/batchExpiryJob.js");
 
 const mongoose = require("mongoose");
 
@@ -8,12 +9,13 @@ exports.cron = async () => {
     try {
 
         connectDB();
+        await scheduleBatchExpiryJob();
         await manageExpiredBatches();
         console.log("Batch expiry job executed successfully.");
-        process.exit(0); // Exit after completion
+        // process.exit(1); // Exit after completion
     } catch (error) {
         console.error("Error during batch expiry job:", error);
-        process.exit(1); // Exit with error
+        // process.exit(0); // Exit with error
     }
 
 };
