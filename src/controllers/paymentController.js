20272f53a893cc8e89445479fea922c12dc0aadb
jayanthbr29 <jayanthbr@digi9.co.in>
2025-01-10 21:22:11 +0530

    payment invoice webhook Test

--- src/controllers/paymentController.js
+++ src/controllers/paymentController.js
@@ -12,6 +12,7 @@ const User = require('../models/userModel');
 const UserNotification = require('../models/userNotificationModel');
 const { studentPaymentRecievedAdmin, studentPaymentRecievedStudent } = require("../mailTemplate/mailTemplates");
 const { sendMailFunctionAdmin, sendMailFunctionTA } = require("../Mail/sendMail");
+const { getInvoicePDF } = require("./invoiceController");
 
 // Create Razorpay Order
 exports.createOrder = async (req, res) => {
@@ -39,7 +40,7 @@ exports.createOrder = async (req, res) => {
 
     // Create Razorpay order
     const orderOptions = {
-      amount: amount * 100, // Amount in paise
+      amount: (Math.ceil(amount)) * 100, // Amount in paise
       currency: "INR",
       receipt: `receipt_${Date.now()}`,
       payment_capture: 1, // Auto-capture
@@ -57,7 +58,7 @@ exports.createOrder = async (req, res) => {
 
     // Save order details in Payment model
     const payment = new Payment({
-      amount: amount,
+      amount: (Math.ceil(amount)),
       currency: order.currency,
       status: "created",
       order_id: order.id,
@@ -180,12 +181,12 @@ exports.verifyPayment = async (req, res) => {
   const generated_signature = crypto.createHmac("sha256", secrete);
   generated_signature.update(JSON.stringify(req.body));
   const digested_signature = generated_signature.digest("hex");
- 
+
   if (digested_signature === signature) {
     if (req.body.event == "payment.captured") {
       console.log("Valid signature inside payment.captured", req.body);
       console.log("request", req.body.payload.payment);
- 
+
       // Payment is valaid
       const payment = await Payment.findOne({
         order_id: req.body.payload.payment.entity.order_id,
@@ -193,7 +194,7 @@ exports.verifyPayment = async (req, res) => {
       if (!payment) {
         return res.status(400).json({ error: "Payment not found" });
       }
- 
+
       // Update payment details
       payment.payment_id = req.body.payload.payment.entity.id;
       payment.status = "paid";
@@ -237,18 +238,18 @@ exports.verifyPayment = async (req, res) => {
         }
         // Update the subdocument fields
         subjectSubdoc.duration = durationInt;
- 
+
         // Calculate new batch_expiry_date: current date + duration months
         const currentDate = new Date();
         const newExpiryDate = new Date(currentDate);
         newExpiryDate.setMonth(newExpiryDate.getMonth() + durationInt);
- 
+
         subjectSubdoc.batch_expiry_date = newExpiryDate;
         subjectSubdoc.batch_status = "active";
- 
+
         // Optionally, set batch_assigned to true if required
         subjectSubdoc.batch_assigned = true;
- 
+
         // Save the updated student document
         await student.save();
         console.log(`Updated subject_id subdoc for student ${student._id}:`, {
@@ -259,31 +260,88 @@ exports.verifyPayment = async (req, res) => {
           batch_status: "active",
         });
       }
- 
+      const studentOne = await Student.findById(payment.student_id)
+        .populate({
+          path: 'subject_id._id',      // The field you want to populate
+          model: 'Subject',            // The model name for the reference
+          select: 'subject_name',      // (optional) which fields to select from Subject
+        })
+        .populate({
+          path: 'subject_id.type_of_batch',
+          model: 'TypeOfBatch',
+          select: 'mode price',        // (optional)
+        })
+        .populate("user_id")
+        .populate("class")
+        .populate("board_id")
+        .populate("type_of_batch");
+      let items = [];
+      // studentOne.map((student) => {
+      studentOne.subject_id.map((data) => {
+        const itemData =
+        {
+          description: data._id.subject_name,
+          price: (data.type_of_batch.price) * data.duration,
+          quantity: 1
+        }
+        items.push(itemData)
+      })
+
+      // })
+      let billTo = {
+        name: studentOne.user_id.name,
+        email: studentOne.user_id.email,
+        phone: studentOne.phone_number,
+      }
+      let invoiceDate = new Date();
+      let invoiceNumber = payment.payment_id;
+      let discount = studentOne.discountAmount
+      const invoiceData = await createInvoice({
+        body: {
+          invoiceNumber,
+          invoiceDate,
+          from,
+          billTo,
+          items,
+          discount
+        }
+      })
+      const subjets = studentOne.subject_id.map((data) => {
+        return data._id.subject_name
+      })
+      const typeofBatchs = subjets = studentOne.subject_id.map((data) => {
+        return data.type_of_batch.mode
+      })
+      const pdfData = await getInvoicePDF(invoiceData._id)
+
+      const html = studentPaymentRecievedStudent(studentOne.user_id.name, studentOne.user_id.email, payment.amount, payment.payment_id, studentOne.board_id.name, studentOne.class.className, subjets, typeofBatchs);
+      // await sendMailFunctionTA(studentOne.user_id.email, 'Subscription Done', html);
+      await sendMailFunctionTA("jayanthbychana@gmail.com", 'Subscription Done', html,pdfData);
+
       // Update student: set is_paid, push payment_id, etc.
       await Student.findByIdAndUpdate(payment.student_id, {
-        $push: { payment_id: payment._id },
+        $push: { payment_id: { _id: payment._id, invoice_id: invoiceData._id } },
         is_paid: true,
       });
     } else if (req.body.event == "payment_link.paid") {
       console.log("Valid signature inside payment.link.paid", req.body);
       console.log("request", req.body.payload.order.entity);
- 
+
       // Payment is valid
       const payment = await Payment.findOne({
         receipt: req.body.payload.order.entity.receipt,
       });
       console.log("payment", payment);
- 
+
       if (!payment) {
         return res.status(400).json({ error: "Payment not found" });
       }
- 
+
       // Update payment details
       payment.payment_id = req.body.payload.payment.entity.id;
       payment.status = "paid";
       await payment.save();
- 
+
       // Update student details
       await Student.findByIdAndUpdate(payment.student_id, {
         $push: {
@@ -293,13 +351,13 @@ exports.verifyPayment = async (req, res) => {
         paymentLink_status: "approved",
       });
       // -------------- END OF NEW CODE -----------------------------------------------
- 
+
       console.log("Payment link paid, custom package updated successfully");
     }
   } else {
     console.log("Invalid signature");
   }
- 
+
   res.json({ status: "ok" });
 };
 
@@ -318,10 +376,10 @@ exports.createCustomPackageOrder = async (req, res) => {
     if (!student) {
       return res.status(404).json({ error: "Student not found" });
     }
-    
-    
+
+
     const orderOptions = {
-      amount: amount * 100, // Amount in paise
+      amount: (Math.ceil(amount)) * 100, // Amount in paise
       currency: "INR",
       receipt: `receipt_${Date.now()}`,
       payment_capture: 1, // Auto-capture
@@ -331,10 +389,9 @@ exports.createCustomPackageOrder = async (req, res) => {
       },
     };
     const order = await razorpayInstance.orders.create(orderOptions);
-
     // Save order details in Payment model
     const payment = new Payment({
-      amount: amount,
+      amount: (Math.ceil(amount)),
       currency: order.currency,
       status: "created",
       order_id: order.id,
@@ -347,7 +404,7 @@ exports.createCustomPackageOrder = async (req, res) => {
     const paymentLinkResponse = await axios.post(
       "https://api.razorpay.com/v1/payment_links",
       {
-        amount: amount * 100, // Amount in paise (1 INR = 100 paise)
+        amount: (Math.ceil(amount)) * 100, // Amount in paise (1 INR = 100 paise)
         currency: "INR",
         accept_partial: true,
         first_min_partial_amount: 100, // Optional, for partial payments
@@ -358,7 +415,7 @@ exports.createCustomPackageOrder = async (req, res) => {
           name: student.user_id.name, // You can replace this with dynamic name if needed
           contact: student.phone_number, // You can replace with dynamic contact if needed
           // email: student.user_id.email, // Student's email
-          email:"jayanthbychana@gmail.com",
+          email: "jayanthbychana@gmail.com",
         },
         notify: {
           sms: true,
@@ -369,7 +426,7 @@ exports.createCustomPackageOrder = async (req, res) => {
           policy_name: "Topper Academy Payament", // Add your custom notes here
         },
         callback_url:
-          "http://localhost:5000/api/payments/customPackage/webhook", // Callback URL
+          "https://www.thetopperacademy.com/", // Callback URL
         callback_method: "get", // HTTP method for the callback
       },
       {
@@ -472,7 +529,7 @@ exports.createOrderRenewal = async (req, res) => {
     });
 
     await payment.save();
-    console.log("rrr",order)
+    console.log("rrr", order)
 
     res.status(200).json(order);
   } catch (error) {
---
+++ src/models/invoiceModel.js
@@ -0,0 +1,68 @@
+const mongoose = require("mongoose");
+
+const invoiceSchema = new mongoose.Schema({
+  invoiceNumber: {
+    type: String,
+    required: true,
+  },
+  invoiceDate: {
+    type: Date,
+    required: true,
+  },
+  from: {
+    type: String, // Can be changed to an object if you want to store more details about the sender
+    default: undefined,
+  },
+  billTo: {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    phone: {
+      type: String,
+      required: true,
+    },
+  },
+  items: [
+    {
+      description: {
+        type: String,
+        required: true,
+      },
+      price: {
+        type: Number,
+        required: true,
+      },
+      quantity: {
+        type: Number,
+        required: true,
+      },
+    },
+  ],
+  discount: {
+    type: Number,
+    required: true,
+  },
+  subtotal: {
+    type: Number,
+    required: true,
+  },
+  gst: {
+    type: Number,
+    required: true,
+  },
+  total: {
+    type: Number,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("Invoice", invoiceSchema);
